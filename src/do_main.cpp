ed6d4f3754e793525dc8d2def6daeba6ad003232
Sam Taylor <samjt775@gmail.com>
2025-01-13 19:46:00 +1000

    feat: convert do_main to esp32

--- src/do_main.cpp
+++ src/do_main.cpp
@@ -1,5 +1,11 @@
 #include "do_main.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
 int now_is_auto_play = 1;
+
 void ui_mian(void)
 {
     ui_init();
@@ -14,19 +20,18 @@ void ui_mian(void)
         {
             while (1)
             {
-                int i = digitalRead(9);
-                delay(20);
+                int i = gpio_get_level(GPIO_NUM_9);
+                vTaskDelay(20 / portTICK_PERIOD_MS);
                 if (i == 0)
                 {
-                   
                     now_is_auto_play = !now_is_auto_play;
-                    Serial.println("now_is_auto_play is changed to " + String(now_is_auto_play));
-                 
+                    ESP_LOGI("AUTO_PLAY", "now_is_auto_play is changed to %d", now_is_auto_play);
                 }
             }
         },
         "lvglpx", 4096, NULL, 1, NULL, 1);
 }
+
 void auto_play(void)
 {
     xTaskCreatePinnedToCore(
@@ -36,54 +41,54 @@ void auto_play(void)
             {
                 if (now_is_auto_play == 0)
                 {
-                    delay(1000);
+                    vTaskDelay(1000 / portTICK_PERIOD_MS);
                     continue;
                 }
                 else
                 {
-                    delay(2000);
+                    vTaskDelay(2000 / portTICK_PERIOD_MS);
                     if (now_is_auto_play == 0)
                     {
                         continue;
                     }
                     _ui_screen_change(ui_Screen4, LV_SCR_LOAD_ANIM_MOVE_LEFT, 500, 0);
-                    delay(3000);
+                    vTaskDelay(3000 / portTICK_PERIOD_MS);
                     if (now_is_auto_play == 0)
                     {
                         continue;
                     }
                     _ui_screen_change(ui_Screen7, LV_SCR_LOAD_ANIM_MOVE_LEFT, 500, 0);
-                    delay(3000);
+                    vTaskDelay(3000 / portTICK_PERIOD_MS);
                     if (now_is_auto_play == 0)
                     {
                         continue;
                     }
                     _ui_screen_change(ui_Screen11, LV_SCR_LOAD_ANIM_MOVE_LEFT, 500, 0);
-                    delay(3000);
+                    vTaskDelay(3000 / portTICK_PERIOD_MS);
                     if (now_is_auto_play == 0)
                     {
                         continue;
                     }
                     _ui_screen_change(ui_Screen6, LV_SCR_LOAD_ANIM_FADE_ON, 500, 0);
-                    delay(3000);
+                    vTaskDelay(3000 / portTICK_PERIOD_MS);
                     if (now_is_auto_play == 0)
                     {
                         continue;
                     }
                     _ui_screen_change(ui_Screen8, LV_SCR_LOAD_ANIM_FADE_ON, 500, 0);
-                    delay(3000);
+                    vTaskDelay(3000 / portTICK_PERIOD_MS);
                     if (now_is_auto_play == 0)
                     {
                         continue;
                     }
                     _ui_screen_change(ui_Screen9, LV_SCR_LOAD_ANIM_FADE_ON, 500, 0);
-                    delay(3000);
+                    vTaskDelay(3000 / portTICK_PERIOD_MS);
                     if (now_is_auto_play == 0)
                     {
                         continue;
                     }
                     _ui_screen_change(ui_Screen10, LV_SCR_LOAD_ANIM_FADE_ON, 500, 0);
-                    delay(3000);
+                    vTaskDelay(3000 / portTICK_PERIOD_MS);
                     if (now_is_auto_play == 0)
                     {
                         continue;
