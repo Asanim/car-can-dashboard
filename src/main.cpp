d361b9610ca0ff54d3be36e5f514ce9741e92c1f
Sam Taylor <samjt775@gmail.com>
2025-01-19 20:11:53 +1000

    feat: Integrate bluetooth example

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,11 @@
 #include "CST816D.h"
 #include "main_task_handler.h"
 #include "WiFi.h"
+#include <BLEDevice.h>
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
 #define I2C_SDA 4
 #define I2C_SCL 5
 #define TP_INT 0
@@ -187,6 +192,21 @@ void setup()
   Serial.println(WiFi.dnsIP());
   Serial.print("状态:");
   Serial.println(WiFi.status());
+
+  BLEDevice::init("ESP32-BLE");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID); 
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                                  CHARACTERISTIC_UUID, 
+                                                  BLECharacteristic::PROPERTY_READ | // 启用读取
+                                                  BLECharacteristic::PROPERTY_WRITE  // 启用写入
+                                                  );
+  pCharacteristic->setValue("Hello World"); // 设置该Characteristic的Value值
+                                            // 如果客户端连上设备后没有任何写入的情况下第一次读取到的数据应该是这里设置的值
+  pService->start(); 
+
+  BLEDevice::startAdvertising();
+
 
 
   // ticker.attach(1, tcr1s);
