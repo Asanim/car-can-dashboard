db08d1bdf060dff1c5669eef94dca3add48b9ff6
Sam Taylor <samjt775@gmail.com>
2025-01-19 19:07:57 +1000

    feat: integrate wifi demo, rename do_mian

--- src/main_original.cpp
+++ src/main_original.cpp
@@ -153,42 +153,6 @@ void setup()
   Serial.begin(115200); /* prepare for possible serial debug */
   Serial.println("I am LVGL_Arduino");
 
-  WiFi.begin("iPhone", "12345678");
-  WiFi.setAutoReconnect(true);
-
-  // Demo connect to wifi network
-  Serial.print("是否连接:");
-  Serial.println(WiFi.isConnected());
-  Serial.print("本地IP:");
-  Serial.println(WiFi.localIP());
-  Serial.print("本地IPv6:");
-  Serial.println(WiFi.localIPv6());
-  Serial.print("mac地址:");
-  Serial.println(WiFi.macAddress());
-  Serial.print("网络ID:");
-  Serial.println(WiFi.networkID());
-  Serial.print("休息:");
-  Serial.println(WiFi.getSleep());
-  Serial.print("获取状态码:");
-  Serial.println(WiFi.getStatusBits());
-  Serial.print("getTxPower:");
-  Serial.println(WiFi.getTxPower());
-  Serial.print("是否自动连接:");
-  Serial.println(WiFi.getAutoConnect());
-  Serial.print("是否自动重连:");
-  Serial.println(WiFi.getAutoReconnect());
-  Serial.print("获取模式:");
-  Serial.println(WiFi.getMode());
-  Serial.print("获取主机名:");
-  Serial.println(WiFi.getHostname());
-  Serial.print("获取网关IP:");
-  Serial.println(WiFi.gatewayIP());
-  Serial.print("dnsIP:");
-  Serial.println(WiFi.dnsIP());
-  Serial.print("状态:");
-  Serial.println(WiFi.status());
-
-
   // ticker.attach(1, tcr1s);
 
   tft.init();
--- src/main_task_handler.cpp
+++ src/main_task_handler.cpp
@@ -2,7 +2,7 @@
 // Obtained from the following URL: http://pan.jczn1688.com/directlink/1/ESP32%20module/1.28inch_ESP32-2424S012.zip
 // This is demo code pre loaded on the development board: https://www.aliexpress.com/item/1005005561489118.html
 
-#include "do_mian.h"
+#include "main_task_handler.h"
 int now_is_auto_play = 1;
 void ui_mian(void)
 {
@@ -98,3 +98,7 @@ void auto_play(void)
         },
         "lvglautoplay", 8192, NULL, 1, NULL, 1);
 }
+
+      cfg.spi_host = SPI2_HOST; // 使用するSPIを選択  ESP32-S2,C3 : SPI2_HOST or SPI3_HOST / ESP32 : VSPI_HOST or HSPI_HOST
+      // ※ ESP-IDFバージョンアップに伴い、VSPI_HOST , HSPI_HOSTの記述は非推奨になるため、エラーが出る場合は代わりにSPI2_HOST , SPI3_HOSTを使用してください。
+      cfg.spi_mode = 0;                  // SPI通信モードを設定 (0 ~ 3)
